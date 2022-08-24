eb84ed1caac9f023d7a15990f393857bf8c1d2cb
reuben-k-o <rubenkhaemba@gmail.com>
2022-08-24 10:43:23 +0300

    corrected a typo

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,11 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 
 router.get("/signup", authController.getSignup);
+
 router.post("/signup", authController.postSignup);
+
 router.get("/login", authController.getLogin);
+
 router.post("/login", authController.postLogin);
 
 module.exports = router;
